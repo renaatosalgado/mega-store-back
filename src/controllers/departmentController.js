719539254410848748764cba0a7b1e0cb19f1c5d
Lucas <lucasazzollinivieira@gmail.com>
2022-02-11 23:46:06 -0300

    feat: department routes done

--- src/controllers/departmentController.js
+++ src/controllers/departmentController.js
@@ -1,14 +1,90 @@
 import db from "../db.js";
 
-export async function cellphonesGet(req, res) {
-    try {
-        const products = db.collection("products").find({ category: "celulares" });
+export async function getCellphones(req, res) {
 
-        if (!products) {
+    try {
+        const cellphones = await db.collection("products").find({ category: "celulares" }).toArray();
+
+        if (!cellphones) {
             return res.sendStatus(404);
         }
 
-        res.status(200).send(products);
+        res.status(200).send(cellphones);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getGames(req, res) {
+    try {
+        const games = await db.collection("products").find({ category: "games" }).toArray();
+
+        if (!games) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(games);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getEletro(req, res) {
+    try {
+        const eletro = await db.collection("products").find({ category: "eletrodomesticos" }).toArray();
+
+        if (!eletro) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(eletro);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getBooks(req, res) {
+    try {
+        const books = await db.collection("products").find({ category: "livros" }).toArray();
+
+        if (!books) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(books);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getFashion(req, res) {
+    try {
+        const fashion = await db.collection("products").find({ category: "moda" }).toArray();
+
+        if (!fashion) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(fashion);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getHomeThings(req, res) {
+    try {
+        const home = await db.collection("products").find({ category: "casa" }).toArray();
+
+        if (!home) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(home);
     } catch (error) {
         console.error(error);
         res.sendStatus(500);
--- src/routes/departmentRouter.js
+++ src/routes/departmentRouter.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { cellphonesGet } from "../controllers/departmentController.js";
+import { getBooks, getCellphones, getEletro, getFashion, getGames, getHomeThings } from "../controllers/departmentController.js";
 
 const departmentRouter = Router();
 
-departmentRouter.get("/celulares", cellphonesGet);
+departmentRouter.get("/cellphones", getCellphones);
+departmentRouter.get("/games", getGames);
+departmentRouter.get("/eletro", getEletro);
+departmentRouter.get("/books", getBooks);
+departmentRouter.get("/fashion", getFashion);
+departmentRouter.get("/home-things", getHomeThings);
 
 export default departmentRouter;
