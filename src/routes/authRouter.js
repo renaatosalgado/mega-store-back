15f3bc1cd3acff7509799cc7feebde9bd2a27570
renaatosalgado <renaato.salgado@gmail.com>
2022-02-13 11:42:08 -0300

    feat: deleting sessions

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { validateSignUp } from "../middleware/validateSignUpMiddleware.js";
 import { validateLogin } from "../middleware/validateLoginMiddleware.js";
-import { login, signUp } from "../controllers/authController.js";
+import { login, signUp, deleteSession } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", validateSignUp, signUp);
 authRouter.post("/login", validateLogin, login);
+authRouter.delete("/delete-session/:tokenNumber", deleteSession);
 
 export default authRouter;
