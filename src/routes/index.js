ddb6270fa341d56f8bcaa0362ca011850e2f33fc
Lucas <lucasazzollinivieira@gmail.com>
2022-02-14 00:59:09 -0300

    fix: reverting to last working build

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,11 @@ import { Router } from "express";
 import authRouter from "./authRouter.js";
 import departmentRouter from "./departmentRouter.js";
 import productsRouter from "./productsRouter.js";
-import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(productsRouter);
 router.use(departmentRouter);
-router.use(userRouter);
 
 export default router;
